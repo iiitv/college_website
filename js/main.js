9d14f6a1ac36d4daff50a653c943952c1379f269
ansarimofid <ansarimofid@gmail.com>
2016-05-11 12:46:36 +0530

    removed the caret

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	$(".button-collapse").sideNav();
 	 $('.collapsible').collapsible();	
+	 $('.dropdown-button').dropdown({
+	       inDuration: 300,
+	       outDuration: 225,
+	       constrain_width: false, // Does not change width of dropdown to that of the activator
+	       hover: true, // Activate on hover
+	       gutter: 0, // Spacing from edge
+	       belowOrigin: true, // Displays dropdown below the button
+	       alignment: 'left' // Displays dropdown with edge aligned to the left of button
+	     }
+	   );
 });
 
-$(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
