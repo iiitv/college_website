71ed826c2a9f2b34c55094c7c7027b76caee47f5
ansarimofid <ansarimofid@gmail.com>
2016-05-15 11:27:44 +0530

    Added homepage with carousel

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
-	$(".button-collapse").sideNav();
-	 $('.collapsible').collapsible();	
-	 $('.dropdown-button').dropdown({
-	       inDuration: 300,
-	       outDuration: 225,
-	       constrain_width: false, // Does not change width of dropdown to that of the activator
-	       hover: true, // Activate on hover
-	       gutter: 0, // Spacing from edge
-	       belowOrigin: true, // Displays dropdown below the button
-	       alignment: 'left' // Displays dropdown with edge aligned to the left of button
-	     }
-	   );
+	$('.carousel').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		autoplay: true,
+  		autoplaySpeed: 2000
+  	});
 });
 
