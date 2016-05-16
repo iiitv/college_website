e96d2f6c00c7caeea621e8d036d73879e87f3ada
ansarimofid <ansarimofid@gmail.com>
2016-05-16 17:48:41 +0530

    Added news section

--- js/main.js
+++ js/main.js
@@ -11,5 +11,6 @@ $(document).ready(function() {
 
   	 // $("#content").jPaginate();
   	 $('#announcement-list').paginate({itemsPerPage: 3});
+  	 $('#news-list').paginate({itemsPerPage: 3});
 });
 
