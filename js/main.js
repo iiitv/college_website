e877ffe87b0633cec28567eb943123aa9bce8f3b
ansarimofid <ansarimofid@gmail.com>
2016-06-18 16:50:58 +0530

    Added contributer list via console

--- js/main.js
+++ js/main.js
@@ -47,3 +47,16 @@ $(document).ready(function() {
     $('#news-list').paginate({ itemsPerPage: 3 });
     $('#events-list').paginate({ itemsPerPage: 3 });
 });
+
+function contributors($arg){
+
+    if ($arg === undefined || $arg == '')
+        console.log("%cContributors List\n\n"+"%c--Designed by--"+
+        "%c\nMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\n\n %c--Core Developers--\n%cMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\nSaurabh Jain (saurabhjn76@gmail.com)\n\n%c--Supporting Developers--\n%cRajuKoushik (g.rajukoushik@gmail.com)\nKamal Awasthi (kamalawasthi97@gmail.com)\nAvi Aryan (avi.aryan123@gmail.com)\n\n%c--Contribution through reporting issues--\n%cHarish Krupo (harishkrupo@gmail.com)\nShalinee Singh (shalinee43@gmail.com)\nMayank Maurya (mayankm522@gmail.com)\nNavneet Nandan (navneet.nandan8@gmail.com)\nPrem Chand Saini (premchandsaini779@gmail.com)\nUnni Rajendra (unni332265@gmail.com)\nRavi Kishan Jha (ravikishan77@gmail.com)\nAnkit Kumar (ankit.kumar071460@yahoo.com)",'color:rgb(163,169,72);text-align:center;font-size:2em','color: rgb(15,108,107);font-size:1.5em;text-align:center','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.2em;','color:rgb(50,50,50);font-size:1.0.5em');
+    else if($arg == 'designer' || $arg =='design')
+        console.log("%c--Designed by--%c\nMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)",'color: rgb(15,108,107);font-size:1.5em;text-align:center','color:rgb(50,50,50);font-size:1.0.5em');
+    else if($arg == 'devs' || $arg == 'dev' || $arg == 'developers' || $arg == 'developer')
+    console.log("%c--Core Developers--\n%cMofid Ansari (ansarimofid@gmail.com)\nPratyush Singh (singh.pratyush96@gmail.com)\nSaurabh Jain (saurabhjn76@gmail.com)\n\n%c--Supporting Developers--\n%cRajuKoushik (g.rajukoushik@gmail.com)\nKamal Awasthi (kamalawasthi97@gmail.com)\nAvi Aryan (avi.aryan123@gmail.com)",'color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);font-size:1.0.5em','color: rgb(15,108,107);font-size:1.5em;','color:rgb(50,50,50);');
+}
+
+console.log("Type %c contributors()%c to get list of contributors",'color:rgb(15,108,107)','color:rgb(50,50,50);');
